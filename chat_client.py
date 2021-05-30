5640e7ce5eca32c3c549a98297709e01dc8f61b3
HiiD <salomone.marco@outlook.it>
2021-05-30 10:54:21 +0200

    Initial commit

---
+++ chat_client.py
@@ -0,0 +1,94 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+import tkinter as tkt
+
+
+def receive_from_server():
+    """Function for handling messages from the server"""
+    while True:
+        try:
+            """Listening for messages from the server"""
+            msg = client_socket.recv(BUFFER_SIZE).decode("utf8")
+            """Push the message to the message list"""
+            window_frame.push_message(msg)
+        except OSError:
+            break
+
+
+def send_to_server(event=None):
+    """Function for sending data to the server"""
+    msg = window_frame.peek_message()
+    window_frame.reset_message()
+    client_socket.send(bytes(msg, "utf8"))
+    if msg == "{quit}":
+        client_socket.close()
+        window_frame.close_window()
+
+
+class TkinterFrame:
+    """Class for handling the main frame"""
+
+    def __init__(self, button_send_action):
+        """Initialize the window"""
+        self.button_send_action = button_send_action
+        self.window = tkt.Tk()
+        self.window.title("Chat Project")
+        self.messages_frame = tkt.Frame(self.window)
+        self.message_property = tkt.StringVar()
+        scrollbar = tkt.Scrollbar(self.messages_frame)
+        self.msg_list = tkt.Listbox(self.messages_frame, yscrollcommand=scrollbar.set)
+        scrollbar.pack(side=tkt.RIGHT, fill=tkt.Y)
+        self.msg_list.pack(side=tkt.LEFT, fill=tkt.BOTH)
+        self.msg_list.pack(anchor="nw")
+        self.messages_frame.pack()
+        self.entry_field = tkt.Entry(self.window, textvariable=self.message_property)
+        self.entry_field.bind("<Return>", button_send_action)
+        self.entry_field.pack()
+        self.send_button = tkt.Button(self.window, text="Send", command=button_send_action)
+        self.send_button.pack()
+        self.window.protocol("WM_DELETE_WINDOW", self.__on_closing)
+
+    def __on_closing(self):
+        """Function invoked when the window is closed"""
+        self.message_property.set("{quit}")
+        self.button_send_action()
+
+    def peek_message(self):
+        """Peek the current message"""
+        return self.message_property.get()
+
+    def reset_message(self):
+        """Reset the message field"""
+        self.message_property.set("")
+
+    def close_window(self):
+        """Close the window"""
+        self.window.quit()
+
+    def push_message(self, message):
+        """Push a message into the message list"""
+        self.msg_list.insert(tkt.END, message)
+
+
+DEFAULT_PORT = 53000
+
+# ----Connection to the Server----
+HOST = input('Write the Host server: ')
+PORT = input(f'Write the Host server port(default is {DEFAULT_PORT}): ')
+if not PORT:
+    PORT = DEFAULT_PORT
+else:
+    PORT = int(PORT)
+
+window_frame = TkinterFrame(send_to_server)
+
+BUFFER_SIZE = 1024
+ADDRESS = (HOST, PORT)
+
+client_socket = socket(AF_INET, SOCK_STREAM)
+client_socket.connect(ADDRESS)
+
+receive_thread = Thread(target=receive_from_server)
+receive_thread.start()
+# Start the app
+tkt.mainloop()
---
+++ chat_server.py
@@ -0,0 +1,73 @@
+from socket import AF_INET, socket, SOCK_STREAM
+from threading import Thread
+
+
+def accept_incoming_connections():
+    """Handle incoming connections to the server"""
+    while True:
+        client, client_address = SERVER.accept()
+        print(f"{client}:{client_address} joined.")
+        client.send(bytes("Write your name, then press Return or click the Send button to join!", "utf8"))
+        addresses[client] = client_address
+        # A thread for each client
+        Thread(target=client_handler, args=(client,)).start()
+
+
+def client_handler(client):
+    """Handles a single client"""
+    name = client.recv(BUFFER_SIZE).decode("utf8")
+    # Welcomes the new user
+    welcome_message = f"Welcome {name}! If you want to quit, write {{quit}}."
+    client.send(bytes(welcome_message, "utf8"))
+    msg = f"{name} joined the chat!"
+    # Broadcast to all the users that a new user just joined the chat
+    broadcast(bytes(msg, "utf8"))
+    # Updates the client dictionary
+    clients[client] = name
+
+    # Listening for new messages from the chat
+    while True:
+        try:
+            msg = client.recv(BUFFER_SIZE)
+            if msg != bytes("{quit}", "utf8"):
+                broadcast(msg, name + ": ")
+            else:
+                client.send(bytes("{quit}", "utf8"))
+                client.close()
+                del clients[client]
+                broadcast(bytes(f"{name} quit.", "utf8"))
+                print(f'{client} disconnected from the chat')
+                break
+        except ConnectionResetError:
+            # Here the client already closed its socket
+            # so this Error is raised because socket.close() cannot be performed
+            del clients[client]
+            broadcast(bytes(f"{name} quit.", "utf8"))
+            print(f'{name} disconnected from the chat')
+            break
+
+
+def broadcast(message, prefix=""):
+    """Broadcast a message to all the clients"""
+    for user in clients:
+        user.send(bytes(prefix, "utf8") + message)
+
+
+clients = {}
+addresses = {}
+
+HOST = 'localhost'
+PORT = 53000
+BUFFER_SIZE = 1024
+ADDRESS = (HOST, PORT)
+
+SERVER = socket(AF_INET, SOCK_STREAM)
+SERVER.bind(ADDRESS)
+
+if __name__ == "__main__":
+    SERVER.listen(5)
+    print("Waiting for connections...")
+    ACCEPT_THREAD = Thread(target=accept_incoming_connections)
+    ACCEPT_THREAD.start()
+    ACCEPT_THREAD.join()
+    SERVER.close()
