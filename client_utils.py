948851169d33947af297d4625488d3cb56b87802
HiiD <salomone.marco@outlook.it>
2021-06-03 12:12:52 +0200

    Added some docs and formatted client_utils

--- client_utils.py
+++ client_utils.py
@@ -2,11 +2,23 @@ from threading import Thread
 from typing import List, Tuple
 from socket import socket, AF_INET, SOCK_STREAM
 
-def create_socket_thread(address: Tuple[str,int], thread_func):
+
+def create_socket_thread(address: Tuple[str, int], thread_func):
+    """Create a socket that connects to the `address` and a thread that handles `thread_func`, then connect the socket
+
+    Parameters
+    ----------
+    address : Tuple[str, int]
+        address used for connecting the socket
+    thread_func : Any
+        function used as the target for the thread
+
+    """
     sock = socket(AF_INET, SOCK_STREAM)
     sock.connect(address)
     sock_thread = Thread(target=thread_func, daemon=True)
     return sock, sock_thread
+
 
 def read_message(sock: socket, buf_size: int = 1024) -> List[str]:
     """Read a message from the server, then return a list of messages sent by the server
